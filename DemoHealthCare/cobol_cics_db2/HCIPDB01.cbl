5d92a48455534cfc4c772071ff5199be1731761e
RegiBrazil <rbarosa@us.ibm.com>
2020-08-10 18:18:51 -0400

    * Changed Aug 10 2020 -   18:18

--- DemoHealthCare/cobol_cics_db2/HCIPDB01.cbl
+++ DemoHealthCare/cobol_cics_db2/HCIPDB01.cbl
@@ -1,7 +1,7 @@
-      ******************************************************************
-      * Used on CICS trx HCAZ                                          *
-      * invoked hen selecting option 2 - Inquire Patient               *
-      * Changed Aug 10 2020 -   18:07
+      *****************************************************************
+      * Used on CICS trx HCAZ
+      * invoked hen selecting option 2 - Inquire Patient
+      * Changed Aug 10 2020 -   18:18
       *  Look for %bug to introdude a  bug  or CA-FIRST-NAME
       ******************************************************************
        IDENTIFICATION DIVISION.
