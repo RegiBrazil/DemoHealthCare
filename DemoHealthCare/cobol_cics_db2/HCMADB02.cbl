c08a768cbb80b2b8e92f035ebf7f70967eb7c504
RegiBrazil <rbarosa@us.ibm.com>
2020-09-01 14:27:29 -0400

    Break the code

--- DemoHealthCare/cobol_cics_db2/HCMADB02.cbl
+++ DemoHealthCare/cobol_cics_db2/HCMADB02.cbl
@@ -1,5 +1,5 @@
       *****************************************************************
-      *  Changed HCMADB02 Aug 25 2020  -  Regi 19:11
+      *  Changed HCMADB02 Sept01 2020  -  Regi 14:26
       * Look for %regi for fixes on abends due missing date
       *   To add patient's name, address and date of birth to the
       *  DB2 patient table creating a new patient entry.
