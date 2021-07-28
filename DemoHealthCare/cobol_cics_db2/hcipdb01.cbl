414ca8e9885983085fc2dcaca44dba7be0db7101
RegiBrazil <rbarosa@us.ibm.com>
2021-07-28 11:24:45 -0400

    * no BUG version of hcipdb01 Changed July 28  2021 -   09:47

--- DemoHealthCare/cobol_cics_db2/hcipdb01.cbl
+++ DemoHealthCare/cobol_cics_db2/hcipdb01.cbl
@@ -1,7 +1,7 @@
       *****************************************************************
       * Used on CICS trx HCAZ
       * invoked cc selecting option 2 - Inquire Patient
-      * hcipdb01 Changed June 21  2021 -   10:05
+      * hcipdb01 Changed July 28  2021 -   09:47
       *  Look for %bug to introduce bugs
       ******************************************************************
        IDENTIFICATION DIVISION.
