a427c9e5f302b12726850a458f19f85a2cc4f401
RegiBrazil <rbarosa@us.ibm.com>
2021-08-05 14:24:33 -0400

    Program is Fixed by Regi - Aug 04

--- DemoHealthCare/cobol_cics_db2/hcipdb01.cbl
+++ DemoHealthCare/cobol_cics_db2/hcipdb01.cbl
@@ -1,7 +1,7 @@
       *****************************************************************
       * Used on CICS trx HCAZ
       * invoked cc selecting option 2 - Inquire Patient
-      * hcipdb01 Changed August 05  2021 -   12:08
+      * hcipdb01 Changed August 05  2021 -   14:23
       *  Look for %bug to introduce bugs
       ******************************************************************
        IDENTIFICATION DIVISION.
