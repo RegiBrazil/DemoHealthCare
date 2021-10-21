69ceede4f8bcc217be68a2ba04fcabc722ffeb80
RegiBrazil <rbarosa@us.ibm.com>
2021-10-21 16:13:09 -0400

    Changed BMS and Program Oct/21

--- DemoHealthCare/cobol_cics_db2/HCMADB02.cbl
+++ DemoHealthCare/cobol_cics_db2/HCMADB02.cbl
@@ -1,5 +1,5 @@
       *****************************************************************
-      *  Changed HCMADB02 Oct 21,2021  -   08:38
+      *  Changed HCMADB02 Oct 21,2021  -   16:12
       * Look for %regi for fixes on abends due missing date
       *   To add patient's name, address and date of birth to the
       *  DB2 patient table creating a new patient entry.
